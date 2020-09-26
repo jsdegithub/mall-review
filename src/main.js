ebf9a9e6b77a8c8fbab73e622d44adf896404c7f
jsdegithub <def_myself@qq.com>
2020-09-26 16:13:58 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 
-import router from './router'
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+axios.interceptors.response.use(response => {
+    let res = response.data;
+    if (res.status == 0) {
+        return res.data;
+    } else if (res.status == 10) {
+        window.location.href = '/#/login';
+    } else {
+        alert(res.msg);
+    }
+})
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);  //将axios挂载到vue实例上，这样就可以直接使用this.axios
+
+Vue.config.productionTip = false  //生产环境下不给任何错误提示
 
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-//由于vue-cli内置的开发服务器是基于nodejs的，所以模块导出规则遵循commenjs规范
+//由于vue-cli内置的开发服务器是基于nodejs的，所以配置文件的导出规则遵循commenjs规范
 module.exports = {
     devServer: {
         host: 'localhost',
         port: 8080,
-        /* proxy: {
+        proxy: {
             '/api': {
-                target: '',
+                target: 'http://mall-pre.springboot.cn',
                 changeOrigin: true,  //如果接口跨域，需要此项配置设置为 true
                 pathRewrite: {
                     '/api': ''
                 }
             }
-        } */
+        }
     },
 
     lintOnSave: false,
