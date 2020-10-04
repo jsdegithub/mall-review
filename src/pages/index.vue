50a3dec4cc39cf8b396f3b906766cc6eb4693e6a
jsdegithub <def_myself@qq.com>
2020-10-04 18:44:42 +0800

    实现首页轮播菜单

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,44 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">手机 电话卡</a>
+                            <div class="children">
+                                <ul v-for="(item, i) in menuList" :key="i">
+                                    <li v-for="(sub, j) in item" :key="j">
+                                        <a :href="sub?'/#/product/'+sub.id:''">
+                                            <img :src="sub?sub.img:'/imgs/item-box-1.png'" />
+                                            {{sub?sub.name:'小米9'}}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电视 盒子</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">笔记本 平板</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">家电 插线板</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">出行 穿戴</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">智能 路由器</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电源 配件</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">生活 箱包</a>
+                        </li>
+                    </ul>
+                </div>
                 <swiper :options="swiperOption">
                     <swiper-slide v-for="(item, index) in slideList" :key="index">
                         <a :href="'/#/product/' + item.id">
@@ -72,19 +110,122 @@
                         img: "/imgs/slider/slide-5.jpg",
                     },
                 ],
+                menuList: [
+                    [
+                        {
+                            id: 30,
+                            img: "/imgs/item-box-1.png",
+                            name: "小米CC9",
+                        },
+                        {
+                            id: 31,
+                            img: "/imgs/item-box-2.png",
+                            name: "小米CC9",
+                        },
+                        {
+                            id: 30,
+                            img: "/imgs/item-box-3.jpg",
+                            name: "RedMi K20 Pro",
+                        },
+                        {
+                            id: 30,
+                            img: "/imgs/item-box-4.jpg",
+                            name: "移动4G专区",
+                        },
+                    ],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                ],
             };
         },
     };
 </script>
 
 <style lang="scss">
+    @import "../assets/scss/mixin.scss";
+    @import "../assets/scss/config.scss";
     .index {
         .swiper-box {
+            position: relative;
+            .nav-menu {
+                position: absolute;
+                left: 0;
+                z-index: 9;
+                width: 264px;
+                height: 451px;
+                padding: 26px 0;
+                background-color: #55585a78;
+                box-sizing: border-box;
+                .menu-wrap {
+                    .menu-item {
+                        height: 50px;
+                        line-height: 50px;
+                        a {
+                            position: relative;
+                            display: block;
+                            font-size: 16px;
+                            color: #ffffff;
+                            padding-left: 30px;
+                            &:after {
+                                position: absolute;
+                                right: 30px;
+                                top: 17.5px;
+                                vertical-align: center;
+                                content: "";
+                                @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+                            }
+                        }
+                        &:hover {
+                            background-color: $colorA;
+                            .children{
+                                display: block;
+                            }
+                        }
+                        .children {
+                            display: none;
+                            width: 962px;
+                            height: 451px;
+                            background-color: $colorG;
+                            position: absolute;
+                            left: 264px;
+                            top: 0;
+                            border: 1px solid $colorH;
+                            ul {
+                                display: flex;
+                                justify-content: space-between;
+                                height: 75px;
+                                li {
+                                    height: 75px;
+                                    line-height: 75px;
+                                    flex: 1;
+                                    padding-left: 23px;
+                                    a {
+                                        color: $colorB;
+                                        font-size: 14px;
+                                        img {
+                                            width: 42px;
+                                            height: 35px;
+                                            vertical-align: middle; //使图片的高度中点与同行文字在同一水平线
+                                            margin-right: 15px;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             .swiper-container {
                 height: 451px;
                 img {
                     width: 100%;
                     height: 100%;
+                }
+                .swiper-button-prev {
+                    left: 274px;
                 }
             }
         }
