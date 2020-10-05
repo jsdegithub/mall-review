be388295621388684cb8213045de059c4707faa2
jsdegithub <def_myself@qq.com>
2020-10-05 22:34:17 +0800

    实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -1,28 +1,32 @@
-import Vue from 'vue'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue";
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import VueLazyLoad from "vue-lazyload";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = "/api";
 axios.defaults.timeout = 8000;
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use((response) => {
     let res = response.data;
     if (res.status == 0) {
         return res.data;
     } else if (res.status == 10) {
-        window.location.href = '/#/login';
+        window.location.href = "/#/login";
     } else {
         alert(res.msg);
     }
-})
+});
 
-Vue.use(VueAxios, axios);  //将axios挂载到vue实例上，这样就可以直接使用this.axios
+Vue.use(VueAxios, axios); //将axios挂载到vue实例上，这样就可以直接使用this.axios
+Vue.use(VueLazyLoad, {
+    loading: "/imgs/loading-svg/loading-bars.svg",
+});
 
-Vue.config.productionTip = false  //生产环境下不给任何错误提示
+Vue.config.productionTip = false; //生产环境下不给任何错误提示
 
 new Vue({
     router,
-    render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
